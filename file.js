fa4c6d4ad6f0f4a32e373d21942b2d9fe6ce36a8
wafiben <wafibenjeddou@gmail.com>
2021-12-20 11:00:15 +0100

    first commit

---
+++ file.js
@@ -0,0 +1,71 @@
+//--------variables
+/* console.log('hello');
+var student='salim';
+let student='salim'
+const student='salim
+
+
+//------loup javascipt without es6
+for(let i=0;i<5;i++){
+  console.log(i)
+}
+for(let i=7;i<10;i++){
+  console.log(i)
+} */
+
+
+//-------------template literal
+/* let name='salem';
+name='taher'
+console.log(name);
+console.log('hello '+name)
+console.log(`hello ${name}`) */
+/* arrayOfNumber.map((elt)=>{
+  console.log(elt)
+}) */
+
+
+//------------map function
+/* arrayOfNumber.map((elt,index)=>{
+    console.log(index)
+    })
+    const arrayOfNumber=[12,13,25,18]
+const x=[1,2,3]
+const arrayOfStudents=[] */
+/* for (let i=0;i<arrayOfNumber.length;i++){
+  console.log(i)
+} */ 
+//---------------
+//----------------arraow function  -spreadoperator
+
+
+
+
+/* const arrayOfUsers=[{id:0,firtsName:'asherf',lastName:'bouzid',age:35},
+{id:1,firtsName:'salim',lastName:'bouzid',age:25},
+{id:0,firtsName:'wissem',lastName:'bouzid',age:30}]
+/* const table=arrayOfUsers.filter((element)=>element.age>25)
+console.log(table)
+console.log(arrayOfUsers)
+const element=arrayOfUsers.find((elt)=>elt.age>25)
+console.log(element)
+arrayOfUsers.push(1)
+console.log(arrayOfUsers) */
+/* console.log(...arrayOfUsers) */
+/* const newTable=[...arrayOfUsers,{id:25,firtsName:'  amin',lastName:'nasri'}]
+console.log(newTable) */; 
+
+/* const person={name:'wafi',age:29}
+
+const newPerson={id:30,...person}
+console.log(newPerson) */
+
+
+//-------------------destructuring
+/* const person={firstName:'ashref',age:35} */
+//--dot notation
+/* console.log(person.firstName */)//ashref//
+//destructuring
+/* const {firstName,age}=person
+console.log(firstName)//ashref
+
